ff6f7bbaa1fe14a6e51685b0b9c39deb6f8fb198
xuzheng <1914171857@qq.com>
2019-05-29 11:05:36 +0800

    优化系统路径

--- starter/src/main/java/com/example/myspringproject/MySpringProjectApplication.java
+++ starter/src/main/java/com/example/myspringproject/MySpringProjectApplication.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.PropertySource;
 
 /**
  * @Author ppbear xuzheng/ppbeartoxuzheng@163.com
@@ -16,7 +15,6 @@ import org.springframework.context.annotation.PropertySource;
 @Slf4j
 @SpringBootApplication(scanBasePackages = "com.example.*")
 @EnableCaching
-@PropertySource("classpath:application.properties")
 public class MySpringProjectApplication {
 
     public static void main(String[] args) {
